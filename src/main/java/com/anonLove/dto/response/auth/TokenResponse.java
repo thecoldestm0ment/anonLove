7123a643d72a7bf8e74a8c6bbf3e8b087789d689
noturavrigk <ksm92038386@gmail.com>
2026-02-05 14:59:10 +0900

    fix: 로그인 api 형식에 맞게 변경

--- src/main/java/com/anonLove/dto/response/auth/TokenResponse.java
+++ src/main/java/com/anonLove/dto/response/auth/TokenResponse.java
@@ -1,15 +1,11 @@
 package com.anonLove.dto.response.auth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class TokenResponse {
-    @JsonProperty("access_token")
     private String accessToken;
-
-    @JsonProperty("refresh_token")
     private String refreshToken;
 }
